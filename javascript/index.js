1a41b342f840d81705373ea0ee7b933ff35dc0a6
Wince Shum <wince.shum@gmail.com>
2020-11-27 22:29:01 -0500

    airplane moving

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,19 @@
-import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
+// import { gsap } from "gsap";
+// import { MotionPathPlugin } from "node_modules/gsap/MotionPathPlugin.js";
 
 gsap.registerPlugin(MotionPathPlugin);
 
-gsap.to("#rect", {
-  duration: 5,
+gsap.set(".plane", {scale: 1, autoAlpha: 1});
+
+gsap.to("#plane", {
+  duration: 30,
   repeat: 12,
   repeatDelay: 3,
   yoyo: true,
   ease: "power1.inOut",
   motionPath:{
-    path: "#path",
-    align: "#path",
+    path: "#planepath",
+    align: "#planepath",
     autoRotate: true,
     alignOrigin: [0.5, 0.5]
   }
