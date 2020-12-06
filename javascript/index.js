f8f9b7b9054471692d0af6250d40916ee4371d95
Wince Shum <wince.shum@gmail.com>
2020-12-06 18:47:32 -0500

    fixed motionpath and scrolltrigger

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,49 @@
 // import { gsap } from "gsap";
-// import { MotionPathPlugin } from "node_modules/gsap/MotionPathPlugin.js";
+// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(MotionPathPlugin);
+var animation;
 
-gsap.set(".plane", {scale: 1, autoAlpha: 1});
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
 
-gsap.to("#plane", {
-  duration: 30,
-  repeat: 12,
-  repeatDelay: 3,
-  yoyo: true,
-  ease: "power1.inOut",
-  motionPath:{
+gsap.set("#paperplane", {autoAlpha: 1});
+gsap.set("#paperplane", {transformOrigin: "50% 50%"});
+
+animation = gsap.to("#paperplane", {
+  scrollTrigger: {
+    trigger: "#planepath",
+    start: "top 30%",
+    end: "bottom 80%",
+    scrub: true,
+    markers: true,
+  //   onUpdate: self => {
+  //     gsap.to("#paperplane", {
+  //       rotation: () => self.direction === 1 ? 0 : -180, overwrite: "auto"
+  //     });
+  //   }
+  },
+  duration: 40,
+  ease: "none",
+  immediateRender: true,
+  motionPath: {
     path: "#planepath",
     align: "#planepath",
+    alignOrigin: [0.5, 0.5],
     autoRotate: true,
-    alignOrigin: [0.5, 0.5]
   }
 });
+
+
+// gsap.to("#plane", {
+//   duration: 20,
+//   // repeat: 12,
+//   // repeatDelay: 3,
+//   // yoyo: true,
+//   ease: "power1.inOut",
+//   motionPath:{
+//     path: "#planepath",
+//     align: "#planepath",
+//     autoRotate: true,
+//     alignOrigin: [0.5, 0.5]
+//   }
+// });
